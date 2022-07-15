a71183f409501cc117218fb2c6b8ee9a8081e060
sharankarthikyan <sharan.ka@zohocorp.com>
2022-07-16 05:00:00 +0530

    make immutable class - adventure game

--- src/Location.java
+++ src/Location.java
@@ -1,3 +1,9 @@
+/*
+* The class objects should the same from the initialization in constructor and remains the same at the end of the program.
+* We are returning a copy of each attributes in all the getter methods, hence it will not change the real one.
+* https://docs.oracle.com/javase/tutorial/essential/concurrency/imstrat.html
+*/
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,28 +14,32 @@ public class Location {
     private final Map<String, Integer> exits;
 
     // Initialization
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<>();
+        if(exits != null) {
+            this.exits = new HashMap<>(exits); // we are creating a new object to make exits immutable.
+        } else {
+            this.exits = new HashMap<>();
+        }
         this.exits.put("Q", 0);
     }
 
     // Getters
     public int getLocationID() {
-        return locationID;
+        return new Integer(locationID); // we are creating a new object to make exits immutable.
     }
 
     public String getDescription() {
-        return description;
+        return new String(description); // we are creating a new object to make exits immutable.
     }
 
     public Map<String, Integer> getExits() {
-        return exits;
+        return new HashMap<>(exits); // we are creating a new object to make exits immutable.
     }
 
     // Behaviours of Location
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 }
