81a615652b78348ee5209a24fdd4ace58c07f2f8
sharankarthikyan <sharan.ka@zohocorp.com>
2022-07-16 04:20:59 +0530

    additional functionality added for adventure game - finding direction by reading sentence or phase

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,13 @@ public class Main {
         locations.get(5).addExit("S", 1);
         locations.get(5).addExit("W", 2);
 
+        Map<String, String> vocabulary = new HashMap<>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
@@ -46,6 +53,16 @@ public class Main {
             System.out.println();
 
             String direction = scanner.nextLine().toUpperCase();
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word: words) {
+                    if(vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
+                }
+            }
+
             if(exits.containsKey(direction)) {
                 loc = exits.get(direction);
             } else {
